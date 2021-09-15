82627732b1c3e21ad0eef1cf8668f087d5ff89c4
Branka Mitrovic <bramit93.gmail.com>
2021-09-15 11:13:17 +0200

    Create toogleable tabs

---
+++ script.js
@@ -0,0 +1,16 @@
+function openTab(event, tabName) {
+    let i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none"
+    }
+
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+
+}
