edda7f8adf9cfa8d93aae7090d882e8497e8db77
Branka Mitrovic <bramit93.gmail.com>
2021-10-05 14:48:00 +0200

    Project upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ let app = {
   containerId: 'movieApp',
   API_KEY: "&apikey=9a1ebd23",
   API_URL: "http://www.omdbapi.com/?",
+  movieDetails: [],
+  currentPage: 1,
+  moviesPerPage: 10,
+  show:  false,
+  lastClickedMovieId: null,
   movieListTemplate: 
   `
     <div id="search-screen" class="tab-content active">
@@ -25,26 +30,28 @@ let app = {
       <button id="getOne-btn">Get one</button>
       <div id="my-movie"></div>
   </div>`.trim(),
-  showStartPage: function() {
-    document.getElementById(this.containerId).innerHTML = this.movieListTemplate;
-  },
   searchPage: function() {
     document.getElementById(this.containerId).innerHTML = this.movieListTemplate;
     document.getElementById('searchPage').onclick = this.searchPage.bind(this);
     document.getElementById('myListPage').onclick = this.myListPage.bind(this);
     document.getElementById('movie-search').onkeyup = this.getMovies.bind(this);
-    document.getElementById('btn-next').onclick = this.nextPage.bind(this);
-    document.getElementById('btn-prev').onclick = this.prevPage.bind(this);
-    document.getElementById('getOne-btn').onclick = this.getOneMovie.bind(this);
+   // document.getElementById('getOne-btn').onclick = this.getOneMovie.bind(this);
+    const btn_next = document.getElementById('btn-next');
+    btn_next.onclick = this.nextPage.bind(this, page);
+    const btn_prev = document.getElementById('btn-prev');
+    btn_prev.onclick = this.prevPage.bind(this, page);
   },
-  getMovies: function(e) {
-    const movie = e.target.value; // add toLowerCase
+
+  getMovies: function() {
+    let movie = document.getElementById("movie-search").value;
+    console.log("movie you are searching for: " + movie);
+    console.log("this currentPage is: " + this.currentPage)
     if (movie.length > 2) {
-      this.loadMovies(movie);
-    //  this.changePage(1);
+      this.fetchData(movie, this.currentPage);
+      this.show = false;
     }
   },
-  getOneMovie: function(movie) {
+ /* getOneMovie: function(movie) {
     document.getElementById(this.containerId).innerHTML = this.moviePageTemplate;
     movie = "Batman"
     const url = this.API_URL + "t=" + movie + this.API_KEY;
@@ -68,10 +75,11 @@ let app = {
       document.getElementById('plot').innerHTML = data.Plot
     })
     .catch((error) => console.error("FETCH ERROR:", error));
-  },
-  loadMovies: function(movie) {
-    const url = this.API_URL + "s=" + movie + this.API_KEY;
-    console.log(url);
+  }, */
+  fetchData: function(movie) {
+    const url = this.API_URL + "s=" + movie + "&page=" + this.currentPage + this.API_KEY;
+    console.log("movie url: " + url);
+  
     fetch(url)
     .then((response) => {
       if (response.ok) {
@@ -82,41 +90,65 @@ let app = {
     })
     .then((data) => {
       console.log(data);
-      this.displayMovies(data); 
+      this.displayMovies(data, this.currentPage);
     })
-    .catch((error) => console.error("FETCH ERROR:", error));
+    .catch((error) => console.error("FETCH ERROR:", error)); 
   },
-  displayMovies: function(data) {
+  
+  displayMovies: function(data, page) {
+    console.log("in display: " + page)
+
+    const movieList = document.getElementById('movie-list');
+    let page_span = document.getElementById("page");
+    movieList.innerHTML = "";
     const movies = data.Search;
     console.log(movies);
     let dataCount = data.totalResults;
-    console.log(dataCount);
-    let numOfPages = this.numOfPages(dataCount);
-    console.log(numOfPages)
-    let firstIndex = this.recordsPerPage * numOfPages;
-    let lastIndex = this.recordsPerPage * (numOfPages + 1) - 1
-    console.log(lastIndex)
-    this.getMovieList(dataCount, movies);
+    console.log("number of Data: " + dataCount);
+    let numOfPages = this.numPages(dataCount, this.moviesPerPage);
+
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numOfPages) page = numOfPages;
+
+    console.log("numOfPages: " + numOfPages);
+    console.log("movies length: " + movies.length);
+
+    for (let i = (page - 1) * this.moviesPerPage; i < (page * this.moviesPerPage) && i < movies.length; i++) {
+      console.log(i);
+      console.log(movies[i]);
+      page_span.innerHTML = page + "/" + numOfPages;
+      movieList.innerHTML += `
+        <li id=${movies[i].imdbID}>
+          <img src=${movies[i].Poster} />
+          <h2>${movies[i].Title}</h2>
+          <h3>${movies[i].Year}<h3>
+        </li>
+       `
+      }
   },
-  getMovieList: function(num, data) {
-    for (i = 0; i <= num; i++) {
-      data.map((movie) => {
-      const dataList = document.getElementById('movie-list');
-      const list_item = document.createElement('li');
-      const image = document.createElement('img');
-      const title = document.createElement('h2');
-      const year = document.createElement('h3');
-      const movieId = `${movie.imdbID}`;
-      year.innerHTML = `${movie.Year}`;
-      title.innerHTML = `${movie.Title}`;
-      image.src = `${movie.Poster}`;
-      list_item.appendChild(image);
-      list_item.appendChild(title);
-      list_item.appendChild(year);
-      dataList.appendChild(list_item);
-      })
+
+  numPages: function(data, records_per_page) {
+      return Math.ceil(data / records_per_page);
+  },
+  nextPage: function(currentPage) {
+      this.currentPage++;
+      console.log(this.currentPage);
+      return this.getMovies(this.currentPage);
+  },
+
+  prevPage: function(currentPage) {
+    if (this.currentPage <= 0) {
+      document.getElementById('btn-prev').disabled = "true";
+      return;
+    } else {
+      this.currentPage--;
+      return this.getMovies(this.currentPage);
+      console.log(this.currentPage);
     }
+    
   },
+
   showMoviePage: function() {
     document.getElementById(this.containerId).innerHTML = this.moviePageTemplate;
     this.getMovieById();
@@ -126,61 +158,6 @@ let app = {
     let link = "http://www.omdbapi.com/?i=" + id + this.API_KEY;
     console.log(link);
   },
-  numOfPages: function(resultsCount) {
-    return Math.ceil(resultsCount / this.recordsPerPage);
-  },
-  changePage: function(page) {
-    console.log(page);
-    const btn_next = document.getElementById("btn-next");
-    const btn_prev = document.getElementById("btn-prev");
-    let page_span = document.getElementById("page");
-
-    // Validate page
-    if(page < 1) {
-      page = 1;
-    } else if (page > this.numPages()) {
-      page = this.numPages();
-    };
-
-    //movieList.innerHTML = "";
-
-    for (let i = (page-1) * this.recordsPerPage; i < (page * this.recordsPerPage); i++) {
-      console.log('lets see');
-    }
-    page_span.innerHTML = page;
-
-    if (page == 1) {
-        btn_prev.disabled = "true";
-    } else {
-        btn_prev.disabled = "false";
-    }
-
-    if (page == this.numPages()) {
-        btn_next.disabled = "true";
-    } else {
-        btn_next.disabled = "false";
-    }
-  },
-
-
-  nextPage: function(page) {
-    let currentPage = page;
-    if (this.currentPage < this.numPages()) {
-        this.currentPage++;
-        this.changePage(currentPage);
-    }
-    console.log(this.currentPage)
-
-  },
-  prevPage: function() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-     this.changePage(this.currentPage);
-    }
-    console.log(this.currentPage);
-
-  },
-
 
   myListPage: function() {
     document.getElementById(this.containerId).innerHTML = this.movieListTemplate;
