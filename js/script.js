1133b23fad35be464f22fdbb5d58cfed0ebda5dc
Branka Mitrovic <bramit93.gmail.com>
2021-10-19 14:42:00 +0200

    Show movie info in modal

--- js/script.js
+++ js/script.js
@@ -21,22 +21,20 @@ let app = {
           <ul id="movie-list" class="movie-list">
           </ul>
       </div>
+
+      <div class="modal" id="myModal"></div>
       
       <div id="page-buttons" class="pagers">
           <button id="btn-prev" class="btn_prev">&#60; Back</button>
-          <button id="btn-next" class="btn_next">Next &#62;</button>
+          <button id="btn-next" class="btn_next">Next &#62;</button><br>
           page: <span id="page"></span>
-
       </div>
-      <button id="getOne-btn">Get one</button>
-      <div id="my-movie"></div>
   </div>`.trim(),
   searchPage: function() {
     document.getElementById(this.containerId).innerHTML = this.movieListTemplate;
     document.getElementById('searchPage').onclick = this.searchPage.bind(this);
     document.getElementById('myListPage').onclick = this.myListPage.bind(this);
     document.getElementById('movie-search').onkeyup = this.getMovies.bind(this);
-   // document.getElementById('getOne-btn').onclick = this.getOneMovie.bind(this);
     const btn_next = document.getElementById('btn-next');
     btn_next.onclick = this.nextPage.bind(this, page);
     const btn_prev = document.getElementById('btn-prev');
@@ -59,31 +57,6 @@ let app = {
       this.show = false;
     }
   },
- /* getOneMovie: function(movie) {
-    document.getElementById(this.containerId).innerHTML = this.moviePageTemplate;
-    movie = "Batman"
-    const url = this.API_URL + "t=" + movie + this.API_KEY;
-    fetch(url)
-    .then((response) => {
-      if (response.ok) {
-        return response.json(); 
-      } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-      }
-    })
-    .then((data) => {
-      console.log(data);
-      document.getElementById('title').innerHTML = data.Title;
-      document.getElementById('year').innerHTML = data.Year;
-      document.getElementById('my-movie'). innerHTML = data.Title;
-      document.getElementById('movie-poster').src = data.Poster;
-      document.getElementById('release').innerHTML = data.Released;
-      document.getElementById('director').innerHTML = data.Director;
-     document.getElementById('actors').innerHTML = data.actors;
-      document.getElementById('plot').innerHTML = data.Plot
-    })
-    .catch((error) => console.error("FETCH ERROR:", error));
-  }, */
   fetchData: function(movie) {
     const url = this.API_URL + "s=" + movie + "&page=" + this.currentPage + this.API_KEY;
     console.log("movie url: " + url);
@@ -103,7 +76,6 @@ let app = {
     })
     .catch((error) => console.error("FETCH ERROR:", error)); 
   },
-  
   displayMovies: function(data, page) {
     console.log("in display: " + page)
     document.getElementById('page-buttons').style.display = "block";
@@ -130,14 +102,55 @@ let app = {
       console.log(i);
       console.log(movies[i]);
       movieList.innerHTML += `
-        <li id=${movies[i].imdbID}>
+        <li id=${movies[i].imdbID} class="list-item" onclick="app.getMovieInfo(this.id)">
           <img src=${movies[i].Poster} />
           <h2>${movies[i].Title}</h2>
           <h3>${movies[i].Year}<h3>
         </li>
        `
-      }
+    }
   },
+  getMovieInfo: function(id) {
+    console.log(id);
+    let movieUrl = "http://www.omdbapi.com/?i=" + id + this.API_KEY;
+    console.log(movieUrl);
+   
+    fetch(movieUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json(); 
+      } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      this.openModal(data);
+    })
+    .catch((error) => console.error("FETCH ERROR:", error)); 
+  },
+  openModal: function(data) {
+    const modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+    modal.innerHTML = `
+    <div class="modal-content">
+      <h1 id="title">${data.Title}</h1>
+      <p id="year">${data.Year}<p>
+      <div class="image-container">
+          <img class="movie-image" src=${data.Poster} id="movie-poster"/>
+      </div>
+      <p class="movie-info" id="release">Released: ${data.Released}</p>
+      <p class="movie-info" id="genre">Genre: ${data.Genre}</p>
+      <p class="movie-info" id="director">Director: ${data.Director}</p>
+      <p class="movie-info" id="actors">Actors: ${data.Actors}</p>
+      <p class="movie-info" id="plot">Plot: ${data.Plot}</p>
+      <div>
+        <button id="movie-page-btn">Add to My List</button>
+      </div>
+  </div>
+  `  
+},
   numPages: function(data, records_per_page) {
       return Math.ceil(data / records_per_page);
   },
@@ -146,7 +159,6 @@ let app = {
       console.log(this.currentPage);
       return this.getMovies(this.currentPage);
   },
-
   prevPage: function(currentPage) {
     if (this.currentPage <= 0) {
       document.getElementById('btn-prev').disabled = "true";
@@ -155,19 +167,7 @@ let app = {
       this.currentPage--;
       return this.getMovies(this.currentPage);
     }
-    
   },
-
-  showMoviePage: function() {
-    document.getElementById(this.containerId).innerHTML = this.moviePageTemplate;
-    this.getMovieById();
-  },
-  getMovieById: function() {
-    console.log("here goes api call");
-    let link = "http://www.omdbapi.com/?i=" + id + this.API_KEY;
-    console.log(link);
-  },
-
   createMyList: function() {
     document.getElementById('search-screen').style.display = 'none';
     let myArr = ["joker", "batman", "monster"];
@@ -175,24 +175,14 @@ let app = {
      console.log(i)
      this.getMovieList(myArr.length, myArr)
     }
-  },
-  moviePageTemplate: 
-  `
-  <div class="movie-screen" id="pageTwo">
-    <h1 id="title">Name</h1>
-    <p id="year">Year<p>
-    <div class="image-container">
-        <img  src="http://lorempixel.com/200/200/nightlif/3" class="movie-image" id="movie-poster"/>
-    </div>
-    <p class="movie-info" id="release">Released:</p>
-    <p class="movie-info" id="genre">Genre:</p>
-    <p class="movie-info" id="director">Director:</p>
-    <p class="movie-info" id="actors">Actors:</p>
-    <p class="movie-info" id="plot">Plot:</p>
-    <div>
-      <button id="movie-page-btn">Add to My List</button>
-    </div>
-  </div>`.trim()
+  }
 }
 
 app.searchPage();
+
+window.onclick = function(event) {
+  let modal = document.getElementById('myModal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
