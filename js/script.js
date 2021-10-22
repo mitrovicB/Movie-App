48b61710dbdca940699cc165876328be66e58504
Branka Mitrovic <bramit93.gmail.com>
2021-10-22 11:59:59 +0200

    check if in favorites

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
+window.onclick = function(event) {
+  let modal = document.getElementById('myModal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 let app = {
   containerId: 'movieApp',
   API_KEY: "&apikey=9a1ebd23",
   API_URL: "http://www.omdbapi.com/?",
-  movieDetails: [],
   currentPage: 1,
   moviesPerPage: 10,
-  show:  false,
-  lastClickedMovieId: null,
-  movieListTemplate: 
-  `
+  isInFavorites: false,
+  movieListTemplate: `
     <div id="search-screen" class="tab-content active">
       <div class="search-bar">
           <div class="input-icons">
@@ -18,40 +22,66 @@ let app = {
       </div>
 
       <div id="movies">
-          <ul id="movie-list" class="movie-list">
-          </ul>
+          <ul id="movie-list" class="movie-list"></ul>
       </div>
 
-      <div class="modal" id="myModal"></div>
-      
       <div id="page-buttons" class="pagers">
           <button id="btn-prev" class="btn_prev">&#60; Back</button>
           <button id="btn-next" class="btn_next">Next &#62;</button><br>
           page: <span id="page"></span>
       </div>
-  </div>`.trim(),
+    </div>`.trim(),
   searchPage: function() {
     document.getElementById(this.containerId).innerHTML = this.movieListTemplate;
     document.getElementById('searchPage').onclick = this.searchPage.bind(this);
     document.getElementById('myListPage').onclick = this.myListPage.bind(this);
     document.getElementById('movie-search').onkeyup = this.getMovies.bind(this);
     const btn_next = document.getElementById('btn-next');
-    btn_next.onclick = this.nextPage.bind(this, page);
+    btn_next.onclick = this.nextPage.bind(this);
     const btn_prev = document.getElementById('btn-prev');
-    btn_prev.onclick = this.prevPage.bind(this, page);
+    btn_prev.onclick = this.prevPage.bind(this);
   },
   myListPage: function() {
     document.getElementById(this.containerId).innerHTML = this.myListTemplate;
+    const myList = document.getElementById('myList');
+
+    let retrievedData = localStorage.getItem("movieID");
+    let favorites = JSON.parse(retrievedData);
+    console.log("retrieved: " + favorites);
+   
+    for (i = 0; i < favorites.length; i++) {
+     console.log(favorites[i]);
+     let movieUrl = "http://www.omdbapi.com/?i=" + favorites[i] + this.API_KEY;
+    console.log(movieUrl);
+    fetch(movieUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json(); 
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      if (data.totalResults === 0) console.log(data.Error);
+      myList.innerHTML += `
+        <li id=${data.imdbID} class="list-item" onclick="app.getMovieInfo(this.id)">
+          <img src=${data.Poster} />
+          <h2>${data.Title}</h2>
+          <h3>${data.Year}<h3>
+        </li>
+      `
+    })
+    .catch((error) => console.error("FETCH ERROR:", error)); 
+    }
   },
   myListTemplate: `
-    <div id="myList">
-      <p>Here goes my list</p>
-    </div>
-  `.trim(),
+      <ul id="myList"></ul>
+    `.trim(),
   getMovies: function() {
     let movie = document.getElementById("movie-search").value;
     console.log("movie you are searching for: " + movie);
-    console.log("this currentPage is: " + this.currentPage)
+    console.log("currentPage is: " + this.currentPage);
     if (movie.length > 2) {
       this.fetchData(movie, this.currentPage);
       this.show = false;
@@ -60,7 +90,6 @@ let app = {
   fetchData: function(movie) {
     const url = this.API_URL + "s=" + movie + "&page=" + this.currentPage + this.API_KEY;
     console.log("movie url: " + url);
-  
     fetch(url)
     .then((response) => {
       if (response.ok) {
@@ -92,7 +121,7 @@ let app = {
     // Validate page
     if (page < 1) page = 1;
     if (page > numOfPages) page = numOfPages;
-    console.log(page)
+
     console.log("numOfPages: " + numOfPages);
     console.log("movies length: " + movies.length);
 
@@ -112,7 +141,7 @@ let app = {
   },
   getMovieInfo: function(id) {
     console.log(id);
-    let movieUrl = "http://www.omdbapi.com/?i=" + id + this.API_KEY;
+    let movieUrl = this.API_URL + "i=" + id + this.API_KEY;
     console.log(movieUrl);
    
     fetch(movieUrl)
@@ -134,55 +163,77 @@ let app = {
     modal.style.display = "block";
 
     modal.innerHTML = `
-    <div class="modal-content">
-      <h1 id="title">${data.Title}</h1>
-      <p id="year">${data.Year}<p>
-      <div class="image-container">
-          <img class="movie-image" src=${data.Poster} id="movie-poster"/>
+      <div class="modal-content">
+        <h1 id="title">${data.Title}</h1>
+        <p id="year">${data.Year}<p>
+        <div class="image-container">
+            <img class="movie-image" src=${data.Poster} id="movie-poster"/>
+        </div>
+        <p class="movie-info" id="release">Released: ${data.Released}</p>
+        <p class="movie-info" id="genre">Genre: ${data.Genre}</p>
+        <p class="movie-info" id="director">Director: ${data.Director}</p>
+        <p class="movie-info" id="actors">Actors: ${data.Actors}</p>
+        <p class="movie-info" id="plot">Plot: ${data.Plot}</p>
+        <div>
+          <button id="my-btn"></button>
+        </div>
       </div>
-      <p class="movie-info" id="release">Released: ${data.Released}</p>
-      <p class="movie-info" id="genre">Genre: ${data.Genre}</p>
-      <p class="movie-info" id="director">Director: ${data.Director}</p>
-      <p class="movie-info" id="actors">Actors: ${data.Actors}</p>
-      <p class="movie-info" id="plot">Plot: ${data.Plot}</p>
-      <div>
-        <button id="movie-page-btn">Add to My List</button>
-      </div>
-  </div>
-  `  
-},
+    `
+  let movieId = data.imdbID;
+  const storageBtn = document.getElementById('my-btn');
+
+    if (this.isInStorage(movieId)) {
+      storageBtn.innerHTML = "Remove";
+      storageBtn.addEventListener('click', () => {
+        this.removeFromFavorites(movieId);
+      })
+    } else {
+      storageBtn.innerHTML = "Add to my List";
+      storageBtn.addEventListener('click', () => {
+        this.saveToFavorites(movieId);
+      });
+    }
+  },
+  isInStorage: function(id) {
+    let movieArr = JSON.parse(localStorage.getItem('movieID'));
+    if (movieArr.includes(id)) return true;
+  },
+  removeFromFavorites: function(id) {
+    let movieArr = JSON.parse(localStorage.getItem('movieID'));
+    let filtered = movieArr.filter(item => item !== id);
+    console.log('filtered: ' + filtered)
+    localStorage.setItem('movieID', JSON.stringify(filtered));
+    document.getElementById('myModal').style.display = "none";
+    this.myListPage();
+  },
+  saveToFavorites: function(id) {
+   let movieArr = JSON.parse(window.localStorage.getItem("movieID")) || [];
+
+    if(movieArr.indexOf(id) == -1 && typeof(Storage) !== "undefined"){
+      movieArr.push(id);
+      localStorage.setItem("movieID", JSON.stringify(movieArr));
+      console.log("movies: " + movieArr);
+    } else {
+      console.log("Sorry, your browser does not support Web Storage...");
+    }
+  },
   numPages: function(data, records_per_page) {
-      return Math.ceil(data / records_per_page);
+    return Math.ceil(data / records_per_page);
   },
   nextPage: function(currentPage) {
-      this.currentPage++;
-      console.log(this.currentPage);
-      return this.getMovies(this.currentPage);
+    this.currentPage++;
+    console.log(this.currentPage);
+    return this.getMovies(this.currentPage);
   },
   prevPage: function(currentPage) {
-    if (this.currentPage <= 0) {
+    if (this.currentPage = 0) {
       document.getElementById('btn-prev').disabled = "true";
       return;
     } else {
       this.currentPage--;
       return this.getMovies(this.currentPage);
     }
-  },
-  createMyList: function() {
-    document.getElementById('search-screen').style.display = 'none';
-    let myArr = ["joker", "batman", "monster"];
-    for (i = 0; i <= myArr.length; i++) {
-     console.log(i)
-     this.getMovieList(myArr.length, myArr)
-    }
   }
 }
 
 app.searchPage();
-
-window.onclick = function(event) {
-  let modal = document.getElementById('myModal');
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
